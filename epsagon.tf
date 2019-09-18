5d60632b69281954308e7af6c958d42f9a2ae894
Tom Isaacson <tom.isaacson@navico.com>
2019-09-18 16:05:34 +1200

    Update epsagon.tf

--- epsagon.tf
+++ epsagon.tf
@@ -1,12 +1,12 @@
 resource "aws_cloudformation_stack" "epsagon-basic" {
-    name = "epsagon-basic"
+  name = "epsagon-basic"
 
-    template_body = "${file("${path.module}/terraform_template.basic.json")}"
-    capabilities = ["CAPABILITY_NAMED_IAM"]
+  template_body = "${file("${path.module}/basic_template.json")}"
+  capabilities = ["CAPABILITY_NAMED_IAM"]
 
-    parameters {
-        AWSAccount         = "${var.epsagon_account_id}"
-        ExternalId         = "${var.epsagon_external_id}"
-        EpsagonSns         = "${var.epsagon_sns_name}"
-    }
+  parameters {
+    AWSAccount         = "${var.epsagon_account_id}"
+    ExternalId         = "${var.epsagon_external_id}"
+    EpsagonSns         = "${var.epsagon_sns_name}"
+  }
 }
